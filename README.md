21d14d547a3eac39a3e9adf6b5c09564e0736840
dsmadhav12 <dsmadhav@hotmail.com>
2025-04-28 18:50:20 -0400

    beta verison of python server

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Sources:
 https://python-socketio.readthedocs.io/en/stable/intro.html#client-examples
 https://www.youtube.com/watch?v=sUzM-vIC-s4
+https://docs.python.org/3/library/asyncio.html
--- server.py
+++ server.py
@@ -2,14 +2,14 @@ import socketio
 import aiohttp
 import socket
 import datetime
-
+from aiohttp import web
+from zoneinfo import ZoneInfo
+import asyncio
+from datetime import datetime
 
 
 Host = "127.0.0.1"
 Port = 65432
-
-from aiohttp import web
-import socketio
 
 sio = socketio.AsyncServer()
 app = web.Application()
@@ -25,15 +25,40 @@ def connect(sid, environ):
     print("connect ", sid)
 
 @sio.event
-async def chat_message(sid, data):
-    print("message ", data)
+async def frame_received(sid,data):
+    est = ZoneInfo("America/New_York")
+    timestamp  = datetime.now(est).isoformat()
+    frame_size = len(data)
+    await sio.emit("frame_feedback",{
+        "timestamp": timestamp,
+        "frame_size": frame_size
+    },room=sid)
+
 
 @sio.event
 def disconnect(sid):
     print('disconnect ', sid)
 
+async def listener():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind((Host, Port))
+    s.setblocking(False)
+    loop = asyncio.get_event_loop()
+    est = ZoneInfo("America/New_York")
+    while True:
+        data, addr = await loop.sock_recvfrom(s, 65535)
+        timestamp = datetime.now(est).isoformat()
+        frame_size = len(data)
+        await sio.emit("frame_feedback",{
+        "timestamp": timestamp,
+        "frame_size": frame_size
+    })
+
 app.router.add_static('/static', 'static')
 app.router.add_get('/', index)
 
-if __name__ == '__main__':
-    web.run_app(app)
+def main():
+    loop = asyncio.get_event_loop()
+    loop.create_task(listener())
+    web.run_app(app, host=Host, port=Port)
+main()
